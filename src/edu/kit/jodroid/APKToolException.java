b1340fe094c86b6b03793992b962f3be92b06351
Martin Mohr <martin.mohr@kit.edu>
2015-04-27 14:01:06 +0200

    add missing class

---
+++ src/edu/kit/jodroid/APKToolException.java
@@ -0,0 +1,14 @@
+package edu.kit.jodroid;
+
+public class APKToolException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3438130718491968868L;
+	
+	public APKToolException(Throwable cause) {
+		super(cause);
+	}
+
+}
