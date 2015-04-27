ef25474b13eb27ec7244ce257ed137fa67b5fde8
Martin Mohr <martin.mohr@kit.edu>
2015-04-27 11:09:05 +0200

    new command-line interface which gets apk file as argument

---
+++ src/edu/kit/jodroid/ShortAnalysis.java
@@ -0,0 +1,28 @@
+package edu.kit.jodroid;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+
+import org.json.JSONException;
+
+import brut.common.BrutException;
+
+import com.ibm.wala.util.CancelException;
+import com.ibm.wala.util.WalaException;
+import com.ibm.wala.util.graph.GraphIntegrity.UnsoundGraphException;
+
+import edu.kit.joana.util.Pair;
+
+public class ShortAnalysis {
+
+	public static void main(String[] args) throws IOException, CancelException, UnsoundGraphException, WalaException, JSONException, InterruptedException, BrutException {
+		AndroidAnalysis a = new AndroidAnalysis();
+		Set<Pair<String, String>> flows = a.runAnalysis(new File(args[0]));
+		System.out.println("found flows:");
+		for (Pair<String, String> f : flows) {
+			System.out.println(String.format("%s --> %s", f.getFirst(), f.getSecond()));
+		}
+	}
+
+}
